a944db69d6ad371c013f289c2ed4a025c752f235
maybeags <suren0709@gmail.com>
2025-04-23 11:26:38 +0900

    feat: leap year task guideline uploaded

--- src/main/java/ch06_condition/Condition05.java
+++ src/main/java/ch06_condition/Condition05.java
@@ -19,5 +19,21 @@ public class Condition05 {
         Scanner scanner = new Scanner(System.in);
         int score;
         String grade;
+
+        System.out.print("점수를 입력하세요 >>> ");
+        score = scanner.nextInt();
+        if(score > 89) {
+            grade = "A";
+        } else if (score > 79) {
+            grade = "B";
+        } else if (score > 69) {
+            grade = "C";
+        } else if (score > 59) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("당신의 점수는 " + score + "점이고, 학점은 " + grade + "입니다.");
     }
 }
