4bc4a5ee324172cfd931114ec180c74084c17cb3
maybeags <suren0709@gmail.com>
2025-04-23 12:32:43 +0900

    feat: 20250423 before switch

--- src/main/java/ch06_condition/Condition08.java
+++ src/main/java/ch06_condition/Condition08.java
@@ -24,5 +24,41 @@ public class Condition08 {
         System.out.print("연도를 입력하세요 >>> ");
         year = scanner.nextInt();
         // 입력 받은 year가 윤년에 해당하는지 판단하는 조건문을 작성하시오.
+        // 오류 발생의 대표적인 사례 ex) year = 400일때
+//        if(year % 4 == 0) {             // 이 조건이 true
+//            if(year % 100 == 0) {       // 근데 얘도 true입니다 그래서 400년은 윤년이 아니라고 출력됨
+//                System.out.println("윤년이 아닙니다.");
+//            } else if(year % 400 == 0) {
+//                System.out.println("윤년입니다.");
+//            }
+//    }
+
+        // 힌트 : 조건은 널널한 것으로부터 촘촘한 것으로 좁혀들어가야 오류가 발생하지 않을 겁니다.
+//        if (year % 400 == 0) {
+//            System.out.println("윤년입니다.");
+//        } else if (year % 100 == 0) {
+//            System.out.println("윤년이 아닙니다.");
+//        } else if (year % 4 == 0) {
+//            System.out.println("윤년입니다.");
+//        } else {                                // 가이드에 안썼지만 4로도 안나누어떨어지면 x윤년
+//            System.out.println("윤년이 아닙니다.");
+//        }
+
+        // sout이 또 반복되는 것 같아서 줄일 수도 있겠죠
+        // 음수값이면 어떻게 처리할지
+        // 더 극단적으로 줄일 방법은 없을지(논리 연산자 사용 / 삼항 연산자 사용)
+
+        // 반복을 줄이기 위한 leapYear 변수를 선언
+        String leapYear = "윤년이 아닙니다";
+
+        if (year % 400 == 0) {
+            leapYear = "윤년입니다.";
+        } else if (year % 100 == 0) {
+        } else if (year % 4 == 0) {
+            leapYear = "윤년입니다.";
+        } else {                                // 가이드에 안썼지만 4로도 안나누어떨어지면 x윤년
+        }
+
+        System.out.println(year + "년은 " + leapYear);
     }
 }
