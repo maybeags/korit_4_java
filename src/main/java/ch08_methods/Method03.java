58ed90a4571aaf85f13be37340b35fbdf33c7fe5
maybeags <suren0709@gmail.com>
2025-04-24 14:15:40 +0900

    feat: 20250424 avg calc guideline

---
+++ src/main/java/ch08_methods/Method03.java
@@ -0,0 +1,20 @@
+package ch08_methods;
+
+import java.util.Scanner;
+
+public class Method03 {
+    public static void main(String[] args) {
+         /*
+            실행 예
+            몇 과목의 점수를 입력하시겠습니까? >>> 3
+            1 과목의 점수를 입력하세요 >>> 100
+            2 과목의 점수를 입력하세요 >>> 90
+            3 과목의 점수를 입력하세요 >>> 95
+            총합은 285.0이며, 평균은 95.0입니다.
+          */
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        double avgScore = 0;
+        int numOfSubs = 0;
+    }
+}
