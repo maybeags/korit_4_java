14255cf1f0aa70c86fb29fe215b413e353d33d16
maybeags <suren0709@gmail.com>
2025-04-25 11:49:02 +0900

    feat: setter / getter

--- src/main/java/ch09_classes/students/Student.java
+++ src/main/java/ch09_classes/students/Student.java
@@ -47,4 +47,41 @@ package ch09_classes.students;
 
  */
 public class Student {
+    // 필드 선언
+    int studentCode;
+    String name;
+    double score;
+    // 생성자들을 정의
+    // 기본 생성자
+    Student(){
+        System.out.println("기본 생성자로 객체가 생성되었습니다.");
+    };
+    // 매개변수 생성자
+    Student(int studentCode){
+        this.studentCode = studentCode;
+        System.out.println("int 매개변수 생성자로 객체가 생성되었습니다.");
+    };
+    Student(String name){
+        this.name = name;
+        System.out.println("String 매개변수 생성자로 객체가 생성되었습니다.");
+    };
+    Student(int studentCode, String name){
+        this.studentCode = studentCode;
+        this.name = name;
+        System.out.println("학번과 이름으로 학생 객체가 생성되었습니다.");
+    };
+    Student(int studentCode, String name, double score){
+        this.studentCode = studentCode;
+        this.name = name;
+        this.score = score;
+        System.out.println("학번 / 이름 / 점수를 등록한 학생 객체가 생성되었습니다.");
+    };
+
+    // showInfo() 메서드 정의
+    void showInfo() {
+        System.out.println("\n학번 : " + studentCode);
+        System.out.println("이름 : " + name);
+        System.out.println("점수 : " + score);
+
+    };
 }
