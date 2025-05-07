b771de70eb811cad9fe306e3ef280339a280837e
maybeags <suren0709@gmail.com>
2025-05-07 14:11:05 +0900

    Feat: gson installation uploaded

--- src/main/java/ch21_json/JSON1.java
+++ src/main/java/ch21_json/JSON1.java
@@ -1,4 +1,19 @@
 package ch21_json;
 
+import com.google.gson.JsonObject;
+
 public class JSON1 {
+    public static void main(String[] args) {
+        // JsonObject 클래스의 인스턴스 생성
+        JsonObject jsonObject1 = new JsonObject();
+        // 속성(key-value entry)을 추가하는 메서드 -> addProperty() 사용합니다.
+        jsonObject1.addProperty("username", 1);
+        jsonObject1.addProperty("password", 1234);
+        jsonObject1.addProperty("email", "a@test.com");
+        jsonObject1.addProperty("name", "김일");
+
+        // 출력
+        System.out.println(jsonObject1);
+
+    }
 }
